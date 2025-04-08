7c0205afb924eafe099ded82dd0ccbf2d266d65a
nik1-ie <niekitaj0805@gmail.com>
2025-04-08 10:33:06 +0200

    file reading automated

--- file_reading.py
+++ file_reading.py
@@ -13,15 +13,38 @@ def get_repo_name(pack):
     Arguments : pack (str) - nom du pack/dossier principal choisi
     Return : appel de la fonction get_files()
     '''
-    return get_files(pack, os.listdir(pack))
+    return os.listdir(pack)
 
-def get_files(pack, reps):
+def get_files(chemin):
     '''
-    Pour chaque dossier, on récupère les noms de fichiers que l'on stocke dans un dictionnaire.
-    Arguments : rep (list) - liste des noms de dossiers
-    Return : (dict) - dictionnaire de noms de fichiers selon le dossier dans lesquels ils se trouvent
+    Pour le dossier donné, on récupère les noms de fichiers que l'on stocke dans une dictionnaire.
+    Arguments : pack (str) - nom de pack
+                rep (str) - nom de dossier
+    Return : (liste) - dictionnaire de noms de fichiers selon le dossier dans lesquels ils se trouvent
     '''
-    files = {}
-    for dossier in reps:
-        files[dossier] = os.listdir(f'{pack}\{dossier}')
+    files = []
+    for name in os.listdir(chemin):
+        files.append(name[:-4])
     return files
+
+def file_dico(pack, dict = {}):
+    '''
+    Fonction récursive créeant directement le dictionnaire.
+    Arguments : pack (str) - chosen pack
+                dict (dict) - dictionnaire en cours de création
+    Return :    dict (dict) - dictionnaire de chemins et de ficheirs complété
+    '''
+    list = get_repo_name(pack)
+    for filename in list:
+        full_path = os.path.join(pack, filename)
+        full_path = os.path.normpath(full_path)
+        if os.path.isdir(full_path):
+            file_dico(full_path)
+        elif os.path.isfile(full_path):
+            dict[os.path.dirname(full_path)] = get_files(os.path.dirname(full_path))
+            return dict
+    return dict
+
+if __name__ == "__main__":
+    files = file_dico('pack1')
+    print(files)
